3223c7bac53d6b889f85da9217f9b20f772c6540
toofaniCoder <smartcoder500@gmail.com>
2023-08-12 09:47:22 +0530

    add a static table UI

--- frontend/src/routes/students.jsx
+++ frontend/src/routes/students.jsx
@@ -1,5 +1,38 @@
+import { Avatar, Box, Button, Sheet, Table } from '@mui/joy';
+
 const Students = () => {
-  return <div>Students</div>;
+  return (
+    <Box sx={{ p: 4 }}>
+      <Sheet sx={{ borderRadius: 10 }}>
+        <Table borderAxis="both" size="lg">
+          <thead>
+            <tr>
+              <th style={{ width: '10%' }}>Profile</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>E-mail Address</th>
+              <th>Phone Number</th>
+              <th style={{ width: '10%' }}>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <Avatar src="https://i.pravatar.cc/300" />
+              </td>
+              <td>Subroto</td>
+              <td>Biswas</td>
+              <td>example@test.com</td>
+              <td>+91 111-1111-111</td>
+              <td>
+                <Button color="danger">delete</Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </Sheet>
+    </Box>
+  );
 };
 
 export default Students;
